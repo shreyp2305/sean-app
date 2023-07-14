02838849d21c821f5e20b8b6a43809cc593236b4
Shrey Patel <118093331+shreyp2305@users.noreply.github.com>
2023-07-14 13:23:17 -0400

    fixed default value for imagePath

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
       allowNull: false,
       required: false,
-      default: ''
+      defaultValue: ''
     }
   },
   {
